f320880d69f702ba3ba79969a62e4546ae5b4690
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2021-02-08 20:46:01 -0300

    adiciona module errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PokemonRoutingModule } from './pokemon/pokemon-routing.module';
 import PokemonModules from './pokemon/pokemon.module';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, PokemonModules, PokemonRoutingModule],
+  declarations: [AppComponent, NotFoundComponent],
+  imports: [BrowserModule, ErrorsModule, PokemonModules, PokemonRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
